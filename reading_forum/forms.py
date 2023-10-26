907d25b18b4afd3f43ec54c9abb39c906b610f2e
syifa dharmawan <dharmawansyifa@gmail.com>
2023-10-26 12:29:49 +0700

    forms.py initialization

---
+++ reading_forum/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Discussion
+
+class DiscussionForm(forms.ModelForm):
+    class Meta:
+        model = Discussion
+        fields = ['title', 'content']
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = Reply
+        fields = ['content']
