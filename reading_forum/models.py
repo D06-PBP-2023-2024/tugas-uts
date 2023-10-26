a02822816331ff77dd701c11f772b49a6fdf30db
syifa dharmawan <dharmawansyifa@gmail.com>
2023-10-26 12:27:55 +0700

    create Discussion and Reply models

--- reading_forum/models.py
+++ reading_forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+## model buat represent struktur data per forum post
+class Discussion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    replies = models.ManyToManyField('Reply', related_name='discussion_replies')
+
+## model buat represent struktur data per reply di certain forum post
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
