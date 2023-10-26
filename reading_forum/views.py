48fbbb94e62d7f585dd41aa83bb7549ebfea4d30
syifa dharmawan <dharmawansyifa@gmail.com>
2023-10-26 12:46:07 +0700

    create functions

--- reading_forum/views.py
+++ reading_forum/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Discussion, Reply
+from .forms import DiscussionForm, ReplyForm
 
-# Create your views here.
+def discussion_list(request):
+    discussions = Discussion.objects.all()
+    return render(request, 'discussion_list.html', {'discussions': discussions})
+
+def discussion_detail(request, discussion_id):
+    discussion = Discussion.objects.get(id=discussion_id)
+    replies = Reply.objects.filter(discussion=discussion)
+    reply_form = ReplyForm()
+
+    if request.method == 'POST':
+        reply_form = ReplyForm(request.POST)
+        if reply_form.is_valid():
+            new_reply = reply_form.save(commit=False)
+            new_reply.user = request.user  # Set the user as the current logged-in user
+            new_reply.discussion = discussion
+            new_reply.save()
+            return redirect('discussion_detail', discussion_id=discussion_id)
+
+    return render(request, 'discussion_detail.html', {'discussion': discussion, 'replies': replies, 'reply_form': reply_form})
+
+def create_discussion(request):
+    if request.method == 'POST':
+        discussion_form = DiscussionForm(request.POST)
+        if discussion_form.is_valid():
+            new_discussion = discussion_form.save(commit=False)
+            new_discussion.user = request.user  # Set the user as the current logged-in user
+            new_discussion.save()
+            return redirect('discussion_list')
+    else:
+        discussion_form = DiscussionForm()
+
+    return render(request, 'discussion_form.html', {'discussion_form': discussion_form})
