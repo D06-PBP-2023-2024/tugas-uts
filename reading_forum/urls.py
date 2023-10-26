8890138f66fe252843b3985d23d0f9d1e6a4c182
syifa dharmawan <dharmawansyifa@gmail.com>
2023-10-27 00:48:06 +0700

    fix filter issue

--- reading_forum/urls.py
+++ reading_forum/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'reading_forum'
+
 urlpatterns = [
     path('', views.discussion_list, name='discussion_list'),
     path('discussion/<int:discussion_id>/', views.discussion_detail, name='discussion_detail'),
@@ -8,5 +10,5 @@ urlpatterns = [
     path('create-discussion/', views.create_discussion, name='create_discussion'),
     path('discussion/<int:discussion_id>/reply/', views.create_reply, name='create_reply'),
     path('reply_form/<int:discussion_id>/', views.reply_form, name='reply_form'),
-    path('', views.discussion_list, name='discussion_list'),
+    
 ]
