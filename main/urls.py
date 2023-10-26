f4012357721852f5b57d721ac237f9bc9dd73256
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2023-10-26 21:54:08 +0700

    Added AJAX feature for search

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from main.views import show_main
 from main.views import search_result
 from main.views import group_tags
 from main.views import search_form
+from main.views import search_result_ajax
 
 app_name = 'main'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('result/', search_result, name='search_result'),
     path('tags/', group_tags, name='group_tags'),
-    path('search/', search_form, name='search_form')
+    path('search/', search_form, name='search_form'),
+    path('search-ajax/', search_result_ajax, name='search_result_ajax'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse
 from main.models import Book, Tag
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
@@ -18,15 +21,15 @@ def search_result(request):
     tags = request.POST.get("tags")
 
     if tags != "":
-        tag = Tag.objects.get(subject=tags)
+        tag = Tag.objects.filter(subject__contains=tags).first()
 
     books = None
    
     if title != "":
         books = Book.objects.filter(title__contains=title)
-    if books is None and tags != "":
+    if books is None and tags != "" and tag is not None:
         books = Book.objects.filter(tags=tag)
-    elif books and tags != "":
+    elif books and tags != "" and tag is not None:
         books = books.filter(tags=tag)
     
     context = {
@@ -57,3 +60,31 @@ def search_form(request):
     context = {}
 
     return render(request, 'search_form.html', context)
+
+@csrf_exempt
+def search_result_ajax(request):
+    title = request.POST.get("title")
+    tags = request.POST.get("tags")
+
+    if tags != "":
+        tag = Tag.objects.filter(subject__contains=tags).first()
+
+    books = None
+   
+    if title != "":
+        books = Book.objects.filter(title__contains=title)
+    if books is None and tags != "" and tag is not None:
+        books = Book.objects.filter(tags=tag)
+    elif books and tags != "" and tag is not None:
+        books = books.filter(tags=tag)
+
+    if books is not None:
+        books = serializers.serialize('json', books)
+    
+    context = {
+        'books': books,
+        'title': title,
+        'tags': tags,
+    }
+
+    return JsonResponse(context)
