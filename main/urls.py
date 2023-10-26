a75b013889b407559b4fa6fc80af286624f35124
juankhusuma <juan.d.khusuma@gmail.com>
2023-10-26 22:51:17 +0700

    added book details

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ import main.views as views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("book/<int:book_id>", views.book_details, name="book_details"),
     path('grouptags/', views.group_tags, name='group_tags'),
     path("api/books", views.get_books, name="get_all_books"),
     path('result/', views.search_result, name='search_result'),
