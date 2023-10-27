c09997c41cdcf7fd7ed285e3bc768cb75a08a195
juankhusuma <juan.d.khusuma@gmail.com>
2023-10-27 16:17:47 +0700

    book details and author details

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ import main.views as views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("author/<int:author_id>", views.author_details, name="author"),
     path("book/<int:book_id>", views.book_details, name="book_details"),
     path("api/books", views.get_books, name="get_all_books"),
     path('result/', views.search_result, name='search_result'),
