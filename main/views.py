cf353e16c02abcc318e895be0281bebdebad309b
juankhusuma <juan.d.khusuma@gmail.com>
2023-10-28 13:46:06 +0700

    pulled from rachel

--- main/views.py
+++ main/views.py
@@ -4,8 +4,7 @@ from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 import django.core.serializers as serializers
-from .forms import CommentForm 
-import requests
+from main.forms import CommentForm 
 
 
 # Create your views here.
