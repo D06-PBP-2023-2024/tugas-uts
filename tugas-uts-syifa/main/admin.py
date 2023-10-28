5f255f0ed00b2ea93fd55b190a0d6b7bfff19fe4
syifa dharmawan <dharmawansyifa@gmail.com>
2023-10-28 16:25:50 +0700

    fix : ajax, form

--- tugas-uts-syifa/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from main.models import Book, Author, Tag
-
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Tag)
--- tugas-uts-syifa/reading_forum/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReadingForumConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reading_forum'
