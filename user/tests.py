c3279053bee66cfbdf10ba0c168cabe850a0a92f
Nya2k <eudoravanya@gmail.com>
2023-10-29 15:39:25 +0700

    tambah test bang

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from main.models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProfileModelTest(TestCase):
+        def test_profile_exist(self):
+                user = User.objects.create_user(username='testuser', password='12345')
+                profile = Profile.objects.create(user=user)
+                profile_exist = Profile.objects.filter(user=user).exists()
+                self.assertTrue(profile_exist)
+
+        def test_profile_not_found(self):
+                response = Client().get('/user/info/unknown/')
+                self.assertEqual(response.status_code, 302)
+                self.assertRedirects(response, '/user/user-not-found')
