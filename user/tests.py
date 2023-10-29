192a580c446060490cfb45f5991cbbaf21621294
rigifdaga <rfdauliaa@gmail.com>
2023-10-29 15:27:51 +0700

    add test.py

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('user:register')
+        self.login_url = reverse('user:login')
+
+    def test_register_POST(self):
+        response = self.client.post(self.register_url, {
+            'username': 'testuser',
+            'password1': 'testing321',
+            'password2': 'testing321'
+        })
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json(), {'success': True})
+
+
+    def test_login_user_POST(self):
+        self.user = User.objects.create_user(username='testuser', password='testing321')
+        
+        response = self.client.post(self.login_url, {
+            'username': 'testuser',
+            'password': 'testing321',
+            'feeling': 'HA'
+        })
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json(), {'success': True})
